d44f5256f09525e59748e138973ec3282731fe5e
Milos Vasic <Milos.Vasic@voiceworks.com>
2018-03-13 14:50:03 +0100

    Echo: Application class.

---
+++ Echo/src/main/kotlin/net/milosvasic/tools/echo/Application.kt
@@ -0,0 +1,11 @@
+package net.milosvasic.tools.echo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
